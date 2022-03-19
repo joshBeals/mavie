6a19ccfeb1a76324eff9d55aa212bd1f44056091
JoshuaBeals <joshua@camelcase.co>
2022-03-19 15:39:27 +0100

    Dashboard design complete

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -7,6 +7,10 @@ import '../../assets/css/style.css';
 import Footer from '../Footer';
 import './Dashboard.css';
 import Spinner from '../Spinner/Spinner';
+import notes from '../../assets/images/notes.png';
+import weight from '../../assets/images/weight.png';
+import memory from '../../assets/images/memory.png';
+import Cards from '../Cards';
 
 const Dashboard = (props) => {
 
@@ -40,10 +44,25 @@ const Dashboard = (props) => {
         navigate('/login');
     }
 
+    const logout = () => {
+        window.localStorage.removeItem('mavie_token');
+        navigate('/login');
+    }
+
     return(
         <div className='main-body'>
             <div className='app-body'>
-                {props.user.data.name}
+                <div className='dashboard-main'>
+                    <h5><strong>Welcome Back, <br /> <span>{props.user.data.name}</span></strong></h5>
+                    <div className='btn-holder'>
+                        <button className='logout' onClick={logout}>Logout</button>
+                    </div>
+                </div>
+                <div className='box-main mt-5'>
+                    <Cards header="Memories" text="Save and share daily memories" color="#f97c4f" img={memory} />
+                    <Cards header="Notes" text="Keep daily notes" color="#5979c3" img={notes} />
+                    <Cards header="Weight Tracker" text="Track your weight daily" color="#18606c" img={weight} />
+                </div>
                 <div className='footer'>
                     <Footer />
                 </div>
