e270f8b6245817171216d48bfd40cad69b5a5775
JoshuaBeals <joshua@camelcase.co>
2022-03-26 09:16:31 +0100

    Optimized for PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
@@ -15,3 +16,8 @@ ReactDOM.render(
     </Provider>,
     document.querySelector('#root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
